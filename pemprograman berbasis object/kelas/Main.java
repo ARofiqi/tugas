934bd0a388d5fdae127bcf2c7a13fb2d76ac8de3
Rofiqi2345 <ahmadrofiqi333@gmail.com>
2024-03-21 12:35:35 +0700

    kelas 6, done

--- pemprograman berbasis object/kelas/Main.java
+++ pemprograman berbasis object/kelas/Main.java
@@ -3,3 +3,8 @@ public class Main{
         System.out.println("Hello World");
     }
 }
+
+public abstract class BangunDatar{
+    public abstract double luas();
+    public abstract double keliling();
+}
---
+++ pemprograman berbasis object/sesi 6/App.java
@@ -0,0 +1,12 @@
+import bangundatar.*;
+import shape.*;
+
+public class App {
+    public static void main(String[] args) {
+        Persegi A = new Persegi(4);
+        Square B = new Square(4);
+        A.getInfo();
+        B.getInfo();
+    }
+    
+}
