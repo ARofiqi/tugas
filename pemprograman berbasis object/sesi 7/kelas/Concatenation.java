352d2b12088024ad128ee15b1b9514c2b9a8fedb
Rofiqi2345 <ahmadrofiqi333@gmail.com>
2024-03-28 12:55:21 +0700

    done

---
+++ pemprograman berbasis object/sesi 7/kelas/Concatenation.java
@@ -0,0 +1,9 @@
+public class Concatenation<A,B,C> {
+    public void add (A a, B b, C c) {
+        System.out.println(a + " " + b + " " + c);
+    }
+
+    public void add (A a, B b) {
+       System.out.println(a + " " + b);
+    }
+}
